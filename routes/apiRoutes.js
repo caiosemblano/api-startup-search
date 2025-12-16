626b9b37243ecb086cb5b9475610c39edbcde6c4
Caio Semblano Silva <semblanocaio@gmail.com>
2025-12-15 22:01:03 -0300

    fix: CRUD completo funcionando

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,6 +2,8 @@ import express from 'express'
 import { getAllData } from '../controllers/getAllData.js'
 import { getDataByPathParams } from '../controllers/getDataByPathParams.js'
 import { createStartup } from '../controllers/createStartup.js'
+import { deleteStartup } from '../controllers/deleteStartup.js'
+import { updateStartup } from '../controllers/updateStartup.js'
 
 export const apiRouter = express.Router()
 
@@ -10,3 +12,8 @@ apiRouter.get('/', getAllData)
 apiRouter.get('/:field/:term', getDataByPathParams)
 
 apiRouter.post('/newStartup', createStartup)
+
+apiRouter.delete('/deleteStartup/:id', deleteStartup)
+
+apiRouter.put('/updateStartup/:id', updateStartup)
+
